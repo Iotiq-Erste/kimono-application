aade530db477afc07af7f25f2900945db7fb9297
ozdkaan <kaan@iotiq.net>
2024-09-23 08:33:12 +0300

    Update SecurityConfiguration.java
    undo

--- src/main/java/com/iotiq/application/config/SecurityConfiguration.java
+++ src/main/java/com/iotiq/application/config/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.iotiq.application.config;
 
 import com.iotiq.application.config.converter.JwtKeycloakAuthConverter;
+import com.iotiq.user.security.jwt.JWTConfigurer;
+import com.iotiq.user.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.List;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -24,18 +25,13 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(request -> {
-                    CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(List.of("*"));
-                    configuration.setAllowedMethods(List.of("*"));
-                    configuration.setAllowedHeaders(List.of("*"));
-                    return configuration;
-                }))
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/swagger-ui/",
+                        .requestMatchers("/swagger-ui/*",
                                 "/v3/api-docs",
-                                "/v3/api-docs/",
-                                "/actuator/")
+                                "/v3/api-docs/*",
+                                "/actuator/**")
+                        .permitAll()
+                        .requestMatchers("/api/v1/auth/*")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
