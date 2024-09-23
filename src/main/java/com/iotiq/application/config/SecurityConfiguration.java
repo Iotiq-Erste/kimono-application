d8130f750f032c7102dd6875f659c147151875a6
ozdkaan <kaan@iotiq.net>
2024-09-23 08:48:44 +0300

    Update SecurityConfiguration.java
    undo

--- src/main/java/com/iotiq/application/config/SecurityConfiguration.java
+++ src/main/java/com/iotiq/application/config/SecurityConfiguration.java
@@ -1,9 +1,9 @@
 package com.iotiq.application.config;
 
 import com.iotiq.application.config.converter.JwtKeycloakAuthConverter;
-import lombok.RequiredArgsConstructor;
 import com.iotiq.user.security.jwt.JWTConfigurer;
 import com.iotiq.user.security.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
