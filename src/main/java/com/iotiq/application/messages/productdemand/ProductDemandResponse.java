21ac8829754638be256e122a7357b649c65572f1
Kaan Özdoğan <kaanozd.x@gmail.com>
2024-10-08 14:43:01 +0300

    medicalData fields were changed

--- src/main/java/com/iotiq/application/messages/productdemand/ProductDemandResponse.java
+++ src/main/java/com/iotiq/application/messages/productdemand/ProductDemandResponse.java
@@ -26,10 +26,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Setter
     public class ProductDemandResponse {
+
+    private UUID id;
+
     private AgeGroup ageGroup;
 
     private ApplicationAreaGroup applicationAreaGroup;
