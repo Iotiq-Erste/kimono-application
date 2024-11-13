156a4908ee0d6d231f7468d655075fff2e364495
kaanozdogan <kaan@iotiq.net>
2024-11-13 11:35:30 +0300

    undo flyway_schema_history creation

--- src/main/resources/db/migration/V1__initial_db_creation.sql
+++ src/main/resources/db/migration/V1__initial_db_creation.sql
@@ -988,22 +988,3 @@ create table if not exists seller_skills
             check ((skills)::text = ANY
         ((ARRAY ['YARN_SUPPLY'::character varying, 'WOVEN_FABRIC'::character varying, 'FINISHING'::character varying, 'PROTOTYPE'::character varying, 'PRODUCT_PARAMETER_MODELING'::character varying, 'MODELING_SUPPORT'::character varying, 'CO_DESIGN'::character varying, 'PATTERN_CREATION'::character varying, 'CUTTING'::character varying, 'ASSEMBLY'::character varying, 'ACTIVE_SUBSTANCE_PLACEMENT'::character varying, 'QUALITY_CONTROL'::character varying, 'B2C'::character varying, 'RECYCLING'::character varying])::text[]))
     );
-
-create table if not exists flyway_schema_history
-(
-    installed_rank integer                 not null
-        constraint flyway_schema_history_pk
-            primary key,
-    version        varchar(50),
-    description    varchar(200)            not null,
-    type           varchar(20)             not null,
-    script         varchar(1000)           not null,
-    checksum       integer,
-    installed_by   varchar(100)            not null,
-    installed_on   timestamp default now() not null,
-    execution_time integer                 not null,
-    success        boolean                 not null
-);
-
-create index if not exists flyway_schema_history_s_idx
-    on flyway_schema_history (success);
