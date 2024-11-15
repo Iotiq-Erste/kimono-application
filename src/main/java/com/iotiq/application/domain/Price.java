88e1ca05ca2515ba50403351ef1e8bdc08d15181
kaanozdogan <kaan@iotiq.net>
2024-11-15 23:11:41 +0300

    price validation

--- src/main/java/com/iotiq/application/domain/Price.java
+++ src/main/java/com/iotiq/application/domain/Price.java
@@ -2,6 +2,8 @@ package com.iotiq.application.domain;
 
 import com.iotiq.application.domain.enums.Currency;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,7 +20,9 @@ import java.math.BigDecimal;
 public class Price {
 
     @NotNull
-    BigDecimal amount;
+    @DecimalMin(value = "0.0", inclusive = false, message = "{exception.greaterThan}")
+    @Digits(integer = 10, fraction = 2, message = "{exception.priceAmountFormat}")
+    private BigDecimal amount;
     @NotNull
     Currency currency;
 }
