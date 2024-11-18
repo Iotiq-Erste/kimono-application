5676599d281bd4b04a193754f45d7fe71cb5259b
kaanozdogan <kaan@iotiq.net>
2024-11-18 15:08:02 +0300

    single imports used

--- src/main/java/com/iotiq/application/messages/product/ProductResponse.java
+++ src/main/java/com/iotiq/application/messages/product/ProductResponse.java
@@ -1,8 +1,28 @@
 package com.iotiq.application.messages.product;
 
+import com.iotiq.application.domain.AgeGroup;
+import com.iotiq.application.domain.ApplicationAreaGroup;
 import com.iotiq.application.domain.Composition;
-import com.iotiq.application.domain.*;
-import com.iotiq.application.domain.enums.*;
+import com.iotiq.application.domain.Design;
+import com.iotiq.application.domain.Haptics;
+import com.iotiq.application.domain.MaterialBehavior;
+import com.iotiq.application.domain.MaterialParameter;
+import com.iotiq.application.domain.Price;
+import com.iotiq.application.domain.Sustainability;
+import com.iotiq.application.domain.enums.Category;
+import com.iotiq.application.domain.enums.Certification;
+import com.iotiq.application.domain.enums.Color;
+import com.iotiq.application.domain.enums.DesignBodyPart;
+import com.iotiq.application.domain.enums.FiberType;
+import com.iotiq.application.domain.enums.Gender;
+import com.iotiq.application.domain.enums.Motif;
+import com.iotiq.application.domain.enums.Neurodermatitis;
+import com.iotiq.application.domain.enums.OekotexStandard;
+import com.iotiq.application.domain.enums.PriceRange;
+import com.iotiq.application.domain.enums.Rating;
+import com.iotiq.application.domain.enums.Size;
+import com.iotiq.application.domain.enums.SpecificBodyPart;
+import com.iotiq.application.domain.enums.SpecificFunctionality;
 import lombok.Getter;
 import lombok.Setter;
 
